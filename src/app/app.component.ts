7d59d1bc892839d3a9a9ab9983439f36ff28b4e1
ryan leslie <ryanleslie33@aol.com>
2019-01-25 14:57:42 -0800

    refactor to create new class in new model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Health } from './models/ohp.model';
+import { Plan } from './models/plans.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ src/app/models/plans.model.ts
@@ -0,0 +1,3 @@
+export class Plan {
+  constructor(public name: string, public phone: number, public monthly: number) { }
+}
