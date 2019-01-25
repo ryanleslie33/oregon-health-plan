d4f6dca125951270a9b049d222cd26b210d10f66
ryan leslie <ryanleslie33@aol.com>
2019-01-25 08:54:02 -0800

    add bootstrap package and boostrap, add formsmodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Health } from './models/ohp.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ src/app/models/ohp.model.ts
@@ -0,0 +1,3 @@
+export class Health {
+  constructor(public name: string) { }
+}
