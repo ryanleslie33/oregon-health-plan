a0561fcaaf731699a6fe84094b0d7d8f2abfe52a
ryan leslie <ryanleslie33@aol.com>
2019-01-25 16:53:45 -0800

    add reset button and continue firebase research

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,30 +12,36 @@ export class AppComponent {
 
   ]
   plans: Plan [] = [
-     new Plan("gold package",5072328888,234.99),
-     new Plan("silver package",433433335,134.99),
-     new Plan("bronze package",434665655,109.99)
+    new Plan("gold package",5072328888,234.99),
+    new Plan("silver package",433433335,134.99),
+    new Plan("bronze package",434665655,109.99)
   ]
 
   selectEnroll = null;
   enrollForm(){
     this.selectEnroll = true;
   }
-selectInfo = null;
+  selectInfo = null;
   showInfo(name,address,salary,coverage) {
     this.selectInfo = true;
 
     let newHealth = new Health(name,address,salary,coverage=true);
 
     if(newHealth.salary > 35000){
-    newHealth.coverage = false;
-
+      newHealth.coverage = false;
     }
     this.health.push(newHealth);
 
   }
+
   selectPlans= null;
-privatePlans(){
-  this.selectPlans = true;
-}
+  privatePlans(){
+    this.selectPlans = true;
+  }
+  startOver(){
+    this.selectEnroll = null;
+    this.selectPlans = null;
+    this.selectInfo = null:
+  }
+
 }
