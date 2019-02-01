3dee59e0c798a4a1c65d9f31aaf30b36fe1b60c3
ryan leslie <ryanleslie33@aol.com>
2019-02-01 08:39:00 -0800

    add form inputs and function to quickstart component

--- src/app/quick-start/quick-start.component.ts
+++ src/app/quick-start/quick-start.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { Router } from '@angular/router';
+import { QuickstartService }   from '../quickstart.service';
+import { Health } from '../models/ohp.model';
 
 @Component({
   selector: 'app-quick-start',
@@ -14,5 +16,10 @@ quickstarts: FirebaseListObservable<any[]>;
 
   ngOnInit() {
   }
+  submitForm(name: string, address: string, salary: number, coverage: boolean) {
+  var newHealth: Health = new Health(name, address, salary,coverage);
+  console.log(newHealth);
+}
+
 
 }
